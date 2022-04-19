b18bb91b1dfbaaab6b4bb58891f1525f04a97167
Jayanta Ramanatha <hrjayanth@gmail.com>
2022-04-19 23:39:44 +0530

    Adding the Team home screen

--- src/app/team/team-home/team-home.component.ts
+++ src/app/team/team-home/team-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../team.service';
 
 @Component({
   selector: 'app-team-home',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamHomeComponent implements OnInit {
 
-  constructor() { }
+  value: any = '';
+  subtab = 1;
+  teams = [{
+    id: 10,
+    name:'ancd'
+  }];
+
+  constructor(private teamService: TeamService) { }
 
   ngOnInit(): void {
+    this.changeTab(1);
   }
 
+  changeTab(tabValue: number) {
+    this.subtab = tabValue;
+    if(tabValue == 1) {
+      this.getTeamList();
+    } else {
+      this.getMyTeamList();
+    }
+  }
+
+  getMyTeamList() {
+    this.teamService.getMyTeams().subscribe(
+      response => {
+        this.teams = response;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  getTeamList() {
+    this.teamService.getTeams().subscribe(
+      response => {
+        this.teams = response;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
---
+++ src/app/team/team.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+
+  getTeamsURL: string = '/teams';
+  getMyTeamsURL: string = '/my-teams';
+  getTeamURL: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  getTeams(): Observable<any> {
+    return this.http.get<Response>(this.getTeamsURL)
+      .pipe(
+        catchError((error: any) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  getMyTeams(): Observable<any> {
+    return this.http.get<Response>(this.getMyTeamsURL)
+      .pipe(
+        catchError((error: any) => {
+          return throwError(error);
+        })
+      );
+  }
+}
